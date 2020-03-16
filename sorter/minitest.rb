34704e953984f8be01c2d202ce2460e507563613
aaaaaaaaaaaaaaaaaaaaa <ozwtkm3062@gmail.com>
2020-03-16 15:47:20 +0900

    ちょいいじった

--- sorter/minitest.rb
+++ sorter/minitest.rb
@@ -1,55 +1,24 @@
 require 'minitest/autorun'
+require 'minitest/mock'
 require './algos'
 
-def train(n)
-  if n == 1
-    nil
-  elsif n == 2
-    ""
-  elsif n == 3
-    "aaaaa"
-  elsif n == 4
-    "exe"
-  else
-    "exea"
-  end
-end
 
-class TrainTest < Minitest::Test
-  def test_train
-    assert_nil train(1)
-    assert_empty train(2)
-    assert_match /[a]+/, train(3)
-    assert_includes ["exe","fe"], train(4)
-    assert_equal 'exea', train(100)
-  end
-end
-
-
-
-
-class Swap!test < Minitest::Test
+class Swap_test < Minitest::Test
 	def setup
-		@sorter = Sore
+		@sorter = Sorter.new
 	end
 
-	def test_swap
-		
+	def test_swap_normal
+		before = @sorter.arr.clone
+		@sorter.swap!(0,1)
+		after = @sorter.arr
+	
+		assert_equal before[0], after[1]
+		assert_equal before[1], after[1]
 	end
-
-
-
-	def swap!(i, j)
-		raise "It is nil" if @arr[i].nil? || @arr[j].nil?
-		
-		tmp = @arr[j]
-		@arr[j] = @arr[i]
-		@arr[i] = tmp
-		
-		@swapped_num += 1
-		
-		puts "((swapped)) " + i.to_s + " & " + j.to_s + " : " + @arr[i].to_s + " , " + @arr[j].to_s + "  ARRAY : " +@arr.to_s
+	
+	def
+	
 	end
 end
-
 
